d59228a67b011df47c7bf6f8bf7a2d8d3cac3847
Ashton B <ashtonbrugh@gmail.com>
2022-03-03 14:33:29 -0500

    module 3

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,6 +4,7 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+    
             me: async (parent, args, context) => {
               if (context.user) {
                 const userData = await User.findOne({ _id: context.user._id })
@@ -73,6 +74,32 @@ const resolvers = {
               }
 
               throw new AuthenticationError('You need to be logged in!');
+          },
+          addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+              if (context.user) {
+                  const updatedThought = await Thought.findOneAndUpdate(
+                      { _id: thoughtId},
+                      { $push: { reactions: { reactionBody, username: context.user.username } } },
+                      { new: true, runValidators: true }
+                  );
+
+                  return updatedThought;
+              }
+
+              throw new AuthenticationError('You need to be logged in!');
+          },
+          addFriend: async (parent, { friendId }, context) => {
+              if (context.user) {
+                  const updatedUser = await User.findOneAndUpdate(
+                      { _id: context.user._id },
+                      { $addToSet: { friends: friendId} },
+                      {  new: true }
+                  ).populate('friends');
+
+                  return updatedUser;
+              }
+
+              throw new AuthenticationError('You need to be logged in!')
           }
     }
 };
